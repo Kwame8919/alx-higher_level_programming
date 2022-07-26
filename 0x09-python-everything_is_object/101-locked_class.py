99d5beb54ed391ef49cdd7dbfce6352509059c44
Kwame8919 <101393080+Kwame8919@users.noreply.github.com>
2022-07-26 21:00:36 +0000

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Module Locked file
+Defines a Locked class.
+"""
 class LockedClass:
-"""Locked Class"""
+"""Locked Class
+"""
     __slots__ = ['first_name']
 
     def __init__(self, first_name=''):
